4e617ed3d3e3dce91f09ff70efad9a044646be19
Eugene Afanasev <emafanasev@yandex.ru>
2020-01-07 15:06:01 +0300

    temporary dummy emoji context

--- permabots/utils.py
+++ permabots/utils.py
@@ -1,12 +1,13 @@
-from telegram import emoji
+# from telegram import emoji
+# TODO: use https://github.com/carpedm20/emoji
 from six import iteritems, PY2
 
 
 def create_emoji_context():
     context = {}
-    for key, value in iteritems(emoji.Emoji.__dict__):
-        if '__' not in key:
-            if PY2:
-                value = value.decode('utf-8')
-            context[key.lower().replace(" ", "_")] = value                    
+    # for key, value in iteritems(emoji.Emoji.__dict__):
+    #     if '__' not in key:
+    #         if PY2:
+    #             value = value.decode('utf-8')
+    #         context[key.lower().replace(" ", "_")] = value
     return context
